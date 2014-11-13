ec605882704b71113b2dfe30f8fcfbd21c2057ca
Jonathan Clem <jonathan@jclem.net>
2014-11-13 16:53:56 -0500

    Add "Disconnect" and "Bad JSON" buttons to sandbox

--- test/sandbox/public/app.js
+++ test/sandbox/public/app.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function onDOMContentLoaded() {
   var elA   = document.querySelector('#area-a');
   var elB   = document.querySelector('#area-b');
+  var disc  = document.querySelector('#disconnect');
+  var err   = document.querySelector('#error');
   var ws    = new WebSocket('ws://' + window.location.host);
   var share = new sharejs.Connection(ws);
   var doc   = share.get('docs', 'sandbox');
+
+
+  disc.addEventListener('click', function onClick() {
+    share.disconnect();
+  });
+
+  err.addEventListener('click', function onClick() {
+    ws.send('Not JSON');
+  });
 
   doc.subscribe();
 
